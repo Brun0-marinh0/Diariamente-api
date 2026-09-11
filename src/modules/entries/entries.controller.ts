81c985b7aa600c8133cd722c6cad75d508ebd088
Brun0-marinh0 <bcaldasmarinhojklb@gmail.com>
2026-09-10 22:21:59 -0300

    feat: add `get` findAllEntries

--- src/modules/entries/entries.controller.ts
+++ src/modules/entries/entries.controller.ts
@@ -1,15 +1,26 @@
-import { Body, Controller, Post, Req} from "@nestjs/common";
+import { Body, Controller, Get, Post, Req} from "@nestjs/common";
 import { CreateEntryUseCase } from "./use-cases/create-entry/create-entry.use-case";
+import { FindEntriesUseCase } from "./use-cases/find-entries/find-entries.use-case";
 import { CreateEntryDto } from "./dto/create-entry.dto";
 import express from "express";
 
 @Controller('entries')
 export class EntriesController {
-    constructor(private createEntryUseCase: CreateEntryUseCase) {}
+    constructor(
+        private createEntryUseCase: CreateEntryUseCase,
+        private findEntriesUseCase: FindEntriesUseCase
+    ) {}
 
     @Post()
     async create(@Body() createEntryDto: CreateEntryDto, @Req() req: express.Request) {
         const userId = (req as any).user.id; // Assuming user ID is attached to the request object
         return this.createEntryUseCase.execute(createEntryDto, userId);
     }
+
+    @Get()
+    async findAll(@Req() req: express.Request) {
+        const userId = (req as any).user.id; // Assuming user ID is attached to the request object
+        return this.findEntriesUseCase.execute(userId);
+    }
+
 }
--- src/modules/entries/entries.module.ts
+++ src/modules/entries/entries.module.ts
@@ -2,10 +2,14 @@ import { Module } from "@nestjs/common";
 import { EntriesController } from "./entries.controller";
 import { EntriesPrismaRepository } from "./repository/implementations/entries.prisma.repository";
 import { CreateEntryUseCase } from "./use-cases/create-entry/create-entry.use-case";
+import { FindEntriesUseCase } from "./use-cases/find-entries/find-entries.use-case";
 
 @Module({
     controllers: [EntriesController],
-    providers: [CreateEntryUseCase, EntriesPrismaRepository,
+    providers: [
+        CreateEntryUseCase,
+        FindEntriesUseCase,
+        EntriesPrismaRepository,
         {provide: 'IEntriesRepository', useExisting: EntriesPrismaRepository},
     ],
     exports: ['IEntriesRepository'],
